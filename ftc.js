96ac34dbf28fb4446c7778dbf91cbfb731442322
BenczeDaniel <94605738+BenczeDaniel@users.noreply.github.com>
2022-04-13 10:25:05 +0200

    Add files via upload

---
+++ ftc.js
@@ -0,0 +1,62 @@
+const kerdesek = [
+    {
+       id:1, 
+      question: "Mikor alapították a Ferencvárosi Torna Club egyesületét?",
+      answers: {
+        a: "1921",
+        b: "1890",
+        c: "1899"
+      },
+      correctAnswer: "c",
+      foto:"letöltés.jpg"
+    },
+    {
+       id:2, 
+      question: "Hány bajnoki címet nyert a Ferencváros egyesülete?",
+      answers: {
+        a: "31",
+        b: "32",
+        c: "33"
+      },
+      correctAnswer: "b",
+      foto:"32.jpg"
+    },
+    {
+       id:3, 
+      question: "Ki a legnagyobb riválisa a Ferencvárosnak?",
+      answers: {
+        a: "Juventus",
+        b: "Kispest",
+        c: "Újpest",
+        d: "MTK"
+      },
+      correctAnswer: "c",
+      foto:"D_KOS20201024012-1-1024x576.jpg"
+    },
+    {
+    id:4, 
+    question: "Hanyas mezszámban játszik Botka Endre?",
+    answers: {
+      a: "21",
+      b: "44",
+      c: "27",
+      d: "15"
+    },
+    correctAnswer: "a",
+    foto:"botka.jpg"
+  },
+  {
+    id:5, 
+    question: "Mikor érkezett elösször a Klubhoz Muhamed Besic?",
+    answers: {
+      a: "2012",
+      b: "2014",
+      c: "2013",
+      d: "2016"
+    },
+    correctAnswer: "a",
+    foto:"momo.jpg"
+  }
+
+
+  ];
